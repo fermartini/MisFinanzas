d493b171ea64e53cb3a42a8a71257a60cf2a02a8
fermartini1 <hola.soymartini1992@gmail.com>
2024-08-29 16:33:17 -0300

    graficos

--- src/components/page/Resumen.jsx
+++ src/components/page/Resumen.jsx
@@ -3,25 +3,81 @@ import { useAuth } from '../../context/authContext';
 import Loading from './Loading';
 import ListaGastos from '../cards/ListaGastos.jsx';
 import Sesion from './Sesion.jsx';
+import Select from '../form/Select.jsx';
 
 export default function Resumen() {
     const {gastos, setGastos,ingresos, setIngresos, nombreGastos,nombreIngresos, loading, authloading, user} = useAuth();
     const [gastosConNombre, setGastosConNombre] = useState([])
     const [ingresosConNombre, setIngresosConNombre] = useState([])
-
-useEffect(() => {
-    
-}, [])
+    const [gastoFiltrado, setGastoFiltrado] = useState([])
+    const [ingresoFiltrado, setIngresoFiltrado] = useState([])
+  
+    const meses = [];
+    const anio = [2024, 2025, 2026]
+  
+    for (let i = 0; i < 12; i++) {
+      const fecha = new Date(2024, i); // Año arbitrario, 2024 en este caso
+      const nombreMes = fecha.toLocaleString('es-ES', { month: 'long' });
+      const numeroMes = i + 1; // Sumamos 1 para que enero sea 1 y diciembre sea 12
+  
+      meses.push({ label: nombreMes, value: numeroMes });
+    }
+    const mes = new Date().toLocaleString('es-ES', { month: 'long' })
+  
+    const mesNumero = new Date().getMonth() + 1
+    const [mesElegido, setMesElegido] = useState({ value: mesNumero, label: mes.toUpperCase() })
+    const [anioElegido, setAnioElegido] = useState({ value: new Date().getFullYear(), label: new Date().getFullYear() })
+  
+    const buscarMesPorValue = (value) => {
+      const mes = meses.find(m => m.value == value);
+      return mes ? mes.label.toUpperCase() : 'Mes no encontrado';
+    };
+  
+  
+    const cambioMes = (e) => {
+      const { name, value } = e.target;
+      setMesElegido({
+        value: parseInt(value),
+        label: buscarMesPorValue(value)
+  
+      })
+    }
+  
+    const cambioAnio = (e) => {
+      const { value } = e.target;
+      setAnioElegido({
+        value: parseInt(value),
+        label: parseInt(value)
+  
+      })
+    }
+  
+  
+  
+  
+    useEffect(() => {
+      if (gastos) {
+        setGastoFiltrado(gastos.filter(gasto => gasto.mes == mesElegido.value && gasto.anio == anioElegido.value))
+  
+      }
+  
+  
+    }, [mesElegido, anioElegido, gastos])
+    useEffect(() => {
+      if (ingresos) {
+        setIngresoFiltrado(ingresos.filter(ingreso => ingreso.mes == mesElegido.value && ingreso.anio == anioElegido.value))
+      }
+      
+  
+    }, [mesElegido, anioElegido, ingresos])
+  
 
     useEffect(() => {
-        gastos ? setGastos(gastos) : setGastos([])
-        ingresos ? setIngresos(ingresos) : setIngresos([])
         if (gastos) {
-            const gastosActualizados = Array.from(new Set(gastos.map(gasto => gasto.nombreGastoId)))
+            const gastosActualizados = Array.from(new Set(gastoFiltrado.map(gasto => gasto.nombreGastoId)))
             .map(id => {
                 const nombreGasto = nombreGastos.find(n => n.id === id);  
-                
-                const total = gastos.reduce((acc, curr) => acc + (curr.nombreGastoId === id ? curr.importe : 0), 0)
+                const total = gastoFiltrado.reduce((acc, curr) => acc + (curr.nombreGastoId === id ? curr.importe : 0), 0)
                 return {
                     id: nombreGasto.id,
                     nombreGasto: nombreGasto.nombre,
@@ -30,9 +86,10 @@ useEffect(() => {
                 };
             });
             setGastosConNombre(gastosActualizados);
+            console.log(gastosConNombre);
         }
         if(ingresos){
-            const ingresosActualizados = Array.from(new Set(ingresos.map(ingreso =>ingreso.nombreIngresoId)))
+            const ingresosActualizados = Array.from(new Set(ingresoFiltrado.map(ingreso =>ingreso.nombreIngresoId)))
             .map(id =>  {
                 const nombreIngreso = nombreIngresos.find(n => n.id === id);  
                 const total = ingresos.reduce((acc, curr) => acc + (curr.nombreIngresoId === id ? curr.importe : 0), 0)              
@@ -45,11 +102,8 @@ useEffect(() => {
             });
             setIngresosConNombre(ingresosActualizados);
         }
-        if(loading || authloading){
-            console.log(gastosConNombre);
-        }
         
-    }, [gastos, ingresos])
+    }, [gastoFiltrado, ingresoFiltrado])
     if (!user) {
         if(loading || authloading){
             return (
@@ -77,18 +131,36 @@ useEffect(() => {
             <div>
                 <h2 className='text-gray-300 text-center' >DETALLES</h2>
             </div>
+            <div className='grid grid-cols-2 gap-10 mb-2 text-center'>
+
+          <Select
+            name={mesElegido.label}
+            value={mesElegido.value}
+            onChange={cambioMes}
+            options={meses.map((mes) => ({ value: mes.value, label: mes.label }))}
+            label='Mes'
+          />
+          <Select
+            name={anioElegido.value}
+            value={anioElegido.value}
+            onChange={cambioAnio}
+            options={anio.map((anio) => ({ value: anio, label: anio }))}
+            label='Año'
+          />
+
+        </div>
             <div className='flex flex-col xl:grid xl:grid-cols-2  gap-10 my-10'>
                 <div>
                     <h2 className='text-3xl text-center text-red-500 '>GASTOS</h2>
                     {loading || authloading ? <div><Loading /></div>  : (gastosConNombre.map((gasto) => (
-                        <ListaGastos icono={gasto.icono} key={gasto.id} importe={gasto.importe}  gastoId={gasto.id} gastoNombre={gasto.nombreGasto}  />
+                        <ListaGastos icono={gasto.icono} key={gasto.id} importe={gasto.importe}  gastoId={gasto.id} gastoNombre={gasto.nombreGasto} gast={gastoFiltrado} />
                     ))
                     )}
                 </div>
                 <div>
                     <h2 className='text-3xl text-center text-green-600 '>INGRESOS</h2>
                     {loading || authloading ? <div><Loading /></div>  : (ingresosConNombre.map((ingreso) => (
-                        <ListaGastos icono={ingreso.icono} key={ingreso.id} importe={ingreso.importe} ingresoId={ingreso.id} gastoNombre={ ingreso.nombreIngreso} />
+                        <ListaGastos icono={ingreso.icono} key={ingreso.id} importe={ingreso.importe} ingresoId={ingreso.id} gastoNombre={ ingreso.nombreIngreso} ingres={ingresoFiltrado} />
                     ))
                     )}
                 </div>
