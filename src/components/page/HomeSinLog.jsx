7857b28ab3ef63d81e3ab24b8d18d508f43562e9
fermartini1 <hola.soymartini1992@gmail.com>
2024-09-05 22:01:37 -0300

    seo mejorado

--- src/components/page/HomeSinLog.jsx
+++ src/components/page/HomeSinLog.jsx
@@ -8,28 +8,32 @@ import Lista from '../iconos/Lista.jsx'
 
 const HomeSinLog = () => {
     return (
-        <section className='w-screen h-fit py-10 px-5 lg:px-52'>
+        <main className='w-screen h-fit py-10 px-5 lg:px-52'>
+            <header>
             <nav className='flex justify-between'>
-                <Link to='/login' className='text-2xl lg:text-5xl'>Mis Finanzas</Link>
+                <Link to='/login' className='text-2xl lg:text-5xl' aria-label='logo mis finanzas'>Mis Finanzas</Link>
                 <div className='flex gap-1 lg:gap-5'> 
-                    <Link to="/login" className='lg:text-lg text-sm bg-purple-700 rounded-xl p-2 lg:p-5 text-white hover:bg-purple-900 hover:text-red-200 transition-colors text-nowrap flex justify-center items-center' aria-label="Iniciar sesión en Finanza Personal">Iniciar sesión</Link>
-                    <Link to="/login" className='lg:text-lg text-sm bg-purple-700 rounded-xl p-2 lg:p-5 text-white hover:bg-purple-900 hover:text-red-200 transition-colors flex justify-center items-center' aria-label="Registrate sesión en Finanza Personal">Registrarse</Link>
+                    <Link to="/login" className='lg:text-lg text-sm bg-purple-700 rounded-xl p-2 lg:p-5 text-white hover:bg-purple-900 hover:text-red-200 transition-colors text-nowrap flex justify-center items-center' aria-label="Iniciar sesión en Mis Finanzas">Iniciar sesión</Link>
+                    <Link to="/login" className='lg:text-lg text-sm bg-purple-700 rounded-xl p-2 lg:p-5 text-white hover:bg-purple-900 hover:text-red-200 transition-colors flex justify-center items-center' aria-label="Registrate sesión en Mis Finanzas">Registrarse</Link>
                 </div>
             </nav>
-            <section className='my-20'>
+            </header>
+           
+            <section className='my-20' aria-labelledby="controla-finanzas">
                 <div className='flex flex-col gap-3 lg:gap-5 justify-center items-center text-white  '>
                     <div className='flex'>
                         <div className='flex flex-col justify-center'>
-                            <h1 className='lg:text-6xl text-3xl text-center font-bold'>Controla tus finanzas con facilidad.</h1>
+                            <h1 id="controla-finanzas" className='lg:text-6xl text-3xl text-center font-bold'>Controla tus finanzas con facilidad.</h1>
                             <h2 className='lg:text-2xl text-sm text-center text-gray-400'>Registra gastos e ingresos, visualiza reportes y toma el control de tu dinero.</h2>
                         </div>
                     </div>
 
 
-                    <Link to='/login' className='text-lg bg-green-500 rounded-xl p-2 lg:p-5 text-blue-950 font-bold hover:bg-green-950 hover:text-red-200 transition-colors'>Comenzar ahora</Link>
+                    <Link to='/login' className='text-lg bg-green-500 rounded-xl p-2 lg:p-5 text-blue-950 font-bold hover:bg-green-950 hover:text-red-200 transition-colors' aria-label="Empezar a gestionar finanzas">Comenzar ahora</Link>
                 </div>
             </section>
-            <section className='grid lg:grid-cols-3 mt-16 gap-10'>
+            <section className='grid lg:grid-cols-3 mt-16 gap-10' aria-labelledby="funcionalidades">
+            <h2 id="funcionalidades" className='sr-only'>Funcionalidades de GastApp</h2>
                 <InfoHome logo={<Dolar style='text-purple-900 h-12 mt-2' />} titulo='Registro de transacciones' descripcion='Agrega facilmente tus gastos e ingresos.' />
                 <InfoHome logo={<Grafico style='text-purple-900 h-16' />} titulo='Graficos intuitivos' descripcion='Visualiza la distribucion de tus gastos e ingresos con graficos claros' />
                 <InfoHome logo={<Analisis style='text-purple-900 h-16 flex justify-center' />} titulo='Análisis  de tendencias' descripcion='Observa cómo evolucionan tus finanzas a lo largo del tiempo.' />
@@ -37,15 +41,15 @@ const HomeSinLog = () => {
                 <InfoHome logo={<Lista style='text-purple-900 h-12 mt-2' />} titulo='Listados detallados' descripcion='Accede a listados completos de todas tus transacciones.' />
 
             </section>
-            <section className='text-white pt-10 flex flex-col items-center gap-5'>
-                <h2 className='lg:text-6xl font-bold text-3xl text-center'>¿Listo para tomar el control de tus finanzas?</h2>
-                <h3 className='lg:text-2xl text-lg text-center'>Únete a miles de usuarios que ya están ahorrando y gestionando mejor su dinero.</h3>
+            <section className='text-white pt-10 flex flex-col items-center gap-5' aria-labelledby="cta">
+                <h3  className='lg:text-6xl font-bold text-3xl text-center'>¿Listo para tomar el control de tus finanzas?</h3>
+                <h2 id="cta" className='lg:text-2xl text-lg text-center'>Únete a miles de usuarios que ya están ahorrando y gestionando mejor su dinero.</h2>
                 <Link to='/login' className='text-lg bg-purple-500 rounded-xl p-5 text-blue-950 font-bold hover:bg-purple-900 hover:text-red-200  transition-colors'>Crear cuenta gratis</Link>
             </section>
             <footer className='container mx-auto px-4 py-8 text-center text-gray-400'>
                 <p className='text-gray-500 text-center text-sm'>© 2024 GastApp. Todos los derechos reservados.</p>
             </footer>
-        </section>
+        </main>
 
     );
 };
--- src/components/page/Sesion.jsx
+++ src/components/page/Sesion.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import GoogleBoton from '../botones/GoogleBoton'
-import { useAuth } from '../../context/authContext'
-import Loading from '../page/Loading'
-import { Link } from 'react-router-dom'
-import FlechaVolver from '../iconos/FlechaVolver'
-
+import React from 'react';
+import GoogleBoton from '../botones/GoogleBoton';
+import { useAuth } from '../../context/authContext';
+import Loading from '../page/Loading';
+import { Link } from 'react-router-dom';
+import FlechaVolver from '../iconos/FlechaVolver';
+import { Helmet } from 'react-helmet';
 
 export default function Sesion() {
-  const { loading, authloading, user } = useAuth()
-  
+  const { loading, authloading, user } = useAuth();
 
-  if (!user)
+  if (!user) {
     return (
-      <>
+      <main>
+        <Helmet>
+          <title>Inicia Sesión o Regístrate - Mis Finanzas App</title>
+          <meta name="description" content="Inicia sesión o regístrate usando Google. Accede a todas las funciones de la aplicación para gestionar tus finanzas de manera eficiente." />
+        </Helmet>
+        
         <div className='mx-10 relative'>
-        <Link to="/" className='bg-gray-700 w-fit absolute top-0 left-0 text-white text-2xl h-20 lg:w-32  my-10  lg:m-10 hover:text-red-500  rounded-xl justify-center items-center flex p-1 shadow-sm shadow-white  active:translate-y-1 transition-all duration-75'>
-        <FlechaVolver style='w-5 mx-5'/>
-        </Link>
+          <Link 
+            to="/" 
+            className='bg-gray-700 w-fit absolute top-0 left-0 text-white text-2xl h-20 lg:w-32 my-10 lg:m-10 hover:text-red-500 rounded-xl justify-center items-center flex p-1 shadow-sm shadow-white active:translate-y-1 transition-all duration-75'
+            aria-label="Volver a la página principal"
+          >
+            <FlechaVolver style='w-5 mx-5' />
+          </Link>
         </div>
-        
-      <form className='h-screen flex justify-center items-center flex-col '>
-        
-        <article className='mb-5 flex justify-center px-7'>
-          <h1 className='bg-gradient-to-t from-[#c7d2fe] to-[#8678f9] bg-clip-text text-xl  text-transparent'>
-            Inicia Sesion / Registrate con:
-          </h1>
-        </article>
-        <div className='bg-blue-950  rounded-xl justify-center items-center flex p-1 shadow-sm shadow-white hover:bg-blue-900 active:bg-blue-950 active:translate-y-1 transition-all duration-75'>
-          <GoogleBoton />
+
+        <div className='h-screen flex justify-center items-center flex-col'>
+          <article className='mb-5 flex justify-center px-7'>
+            <h1 className='bg-gradient-to-t from-[#c7d2fe] to-[#8678f9] bg-clip-text text-xl text-transparent'>
+              Inicia Sesión / Regístrate con:
+            </h1>
+          </article>
+          <div className='bg-blue-950 rounded-xl justify-center items-center flex p-1 shadow-sm shadow-white hover:bg-blue-900 active:bg-blue-950 active:translate-y-1 transition-all duration-75'>
+            <GoogleBoton />
+          </div>
         </div>
-        
-        
-      </form>
-      
-      </>)
-      if(!loading || !authloading) return (
-        <div className='h-screen w-screen flex flex-col justify-center items-center text-yellow-100 text-5xl'><Loading/> </div>)
+      </main>
+    );
+  }
+
+  if (!loading || !authloading) {
+    return (
+      <div className='h-screen w-screen flex flex-col justify-center items-center text-yellow-100 text-5xl'>
+        <Loading />
+      </div>
+    );
+  }
+
+  return null;
 }
